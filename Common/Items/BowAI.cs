e432c464f1e3305bc589715a6b4cc64a840104db
csadwgfsrhg <75584350+csadwgfsrhg@users.noreply.github.com>
2025-04-09 09:57:58 -0700

    o

--- Common/Items/BowAI.cs
+++ Common/Items/BowAI.cs
@@ -9,94 +9,9 @@ using System.Collections.Generic;
 
 namespace Eclipse.Common.Items;
 
-public abstract class BowHeld : ModProjectile
-{
-
-    public override void SetDefaults()
-    {
-        Projectile.width = 50;
-        Projectile.height = 50;
-        Projectile.friendly = false;
-        Projectile.tileCollide = false;
-        Projectile.penetrate = -1;
-
-        Projectile.aiStyle = -1;
-        Projectile.hide = true;
-    }
-
-
-    public override void AI()
-    {
-        Player player = Main.player[Projectile.owner];
-
-        Projectile.timeLeft = 60;
-
-        
-        Vector2 playerCenter = player.RotatedRelativePoint(player.MountedCenter);
-        if (Main.myPlayer == Projectile.owner)
-        {
-
-            if (player.channel ||  Projectile.ai[0] <= 20)
-            {
-                Projectile.NewProjectile(player.GetSource_FromThis(), player.Center, (Projectile.velocity / 2 + (Projectile.velocity * (Projectile.ai[0] / 180))),ModContent.ProjectileType<Trajectory>(), 0, 0f, player.whoAmI, ai2: Projectile.ai[2]);
-            
-                if (Projectile.ai[0] < 80)
-                {
-                    Projectile.ai[0] += 1;
-                }
-               
-                if (Projectile.ai[0] == 79)
-                {
-                    SoundEngine.PlaySound(SoundID.Item4);
-                }
-                float holdoutDistance = player.HeldItem.shootSpeed * Projectile.scale;
-
-                Vector2 holdoutOffset = holdoutDistance * Vector2.Normalize(Main.MouseWorld - playerCenter);
-                if (holdoutOffset.X != Projectile.velocity.X || holdoutOffset.Y != Projectile.velocity.Y)
-                {
-
-                    Projectile.netUpdate = true;
-                }
-
-
-                Projectile.velocity = holdoutOffset;
-            }
-            else
-            {
-              
-
-                SoundEngine.PlaySound(SoundID.Item5 with { Pitch = ( -1/4 + (Projectile.ai[0] / 120)), Volume = (Projectile.ai[0] / 30) });
-                //ammo conversion?
-                Projectile.NewProjectile(player.GetSource_FromThis(), player.Center, (Projectile.velocity / 2 + (Projectile.velocity * (Projectile.ai[0] / 180)) ) , (int)Projectile.ai[2], (int)(Projectile.damage /2 + (Projectile.damage  * (Projectile.ai[0] / 45))), 0f, player.whoAmI, player.whoAmI);
-                Projectile.Kill();
-
-            }
-        }
-
-        if (Projectile.velocity.X > 0f)
-        {
-            player.ChangeDir(1);
-        }
-        else if (Projectile.velocity.X < 0f)
-        {
-            player.ChangeDir(-1);
-        }
-
-        Projectile.spriteDirection = Projectile.direction;
-        player.ChangeDir(Projectile.direction); 
-        player.heldProj = Projectile.whoAmI; 
-        player.SetDummyItemTime(2); 
-        Projectile.Center = playerCenter; 
-        Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
-        player.itemRotation = (Projectile.velocity * Projectile.direction).ToRotation();
-
-
-
-    }
-
-}
 internal class TestBow : GlobalItem
 {
+
     public static List<int> BowsToOverride = new List<int>()
         {
                 ItemID.DD2BetsyBow,
@@ -131,13 +46,13 @@ internal class TestBow : GlobalItem
         };
     public override void SetDefaults(Item Item)
     {
-        
+
 
         if (BowsToOverride.Contains(Item.type))
         {
             //max charge
             //arrow amnt
-            //minimum charge
+
             //auto release bool
             //Item.useAmmo = AmmoID.Arrow;
             //Item.useAmmo = AmmoID.Arrow;
@@ -148,6 +63,7 @@ internal class TestBow : GlobalItem
             //Item.damage = 6;
             Item.UseSound = SoundID.Item10; //todo custom sound stylez
             Item.noMelee = true;
+            Item.autoReuse = false;
             Item.channel = true;
         }
     }
@@ -155,9 +71,15 @@ internal class TestBow : GlobalItem
     {
         if (BowsToOverride.Contains(item.type))
         {
-            Projectile.NewProjectile(source, player.Center, new Vector2(MathF.Pow(velocity.X, 8.2f), MathF.Pow(velocity.Y, 8.2f)), ModContent.ProjectileType<TestBowHeld>(), damage, knockback, player.whoAmI, ai2: type);
+            if (item.type == ItemID.IceBow)
+            {
+
+           //     player.GetModPlayer<Bowplayer>().charge = 2;
+            }
+            Projectile.NewProjectile(source, player.Center, new Vector2(MathF.Pow(velocity.X, 8.2f), MathF.Pow(velocity.Y, 8.2f)), ModContent.ProjectileType<TestBowHeld>(), damage, knockback, player.whoAmI, ai2: type );
             return false;
         }
+      
         return true;
     }
 
@@ -168,10 +90,96 @@ internal class TestBow : GlobalItem
 }
 internal class TestBowHeld : BowHeld
 {
-    
+
     public override string Texture => "Eclipse/Content/Projectiles/Magic/LifeCrystalStaffHeld";
     public override void SetStaticDefaults()
     {
 
     }
+}
+public abstract class BowHeld : ModProjectile
+{
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 50;
+        Projectile.height = 50;
+        Projectile.friendly = false;
+        Projectile.tileCollide = false;
+        Projectile.penetrate = -1;
+
+        Projectile.aiStyle = -1;
+        Projectile.hide = true;
+    }
+
+    
+    public override void AI()
+    {
+        Player player = Main.player[Projectile.owner];
+
+        Projectile.timeLeft = 60;
+
+        
+        Vector2 playerCenter = player.RotatedRelativePoint(player.MountedCenter);
+        if (Main.myPlayer == Projectile.owner)
+        {
+
+            if (player.channel ||  Projectile.ai[0] <= 20)
+            {
+                Projectile.NewProjectile(player.GetSource_FromThis(), player.Center, (Projectile.velocity / 2 + (Projectile.velocity * (Projectile.ai[0] / 120))),ModContent.ProjectileType<Trajectory>(), 0, 0f, player.whoAmI, ai2: Projectile.ai[2]);
+            
+                if (Projectile.ai[0] < 80)
+                {
+                    Projectile.ai[0] += 1; //player.GetModPlayer<Bowplayer>().charge;
+                }
+               
+                if (Projectile.ai[0] == 79)
+                {
+                    SoundEngine.PlaySound(SoundID.Item4);
+                }
+                float holdoutDistance = player.HeldItem.shootSpeed * Projectile.scale;
+
+                Vector2 holdoutOffset = holdoutDistance * Vector2.Normalize(Main.MouseWorld - playerCenter);
+                if (holdoutOffset.X != Projectile.velocity.X || holdoutOffset.Y != Projectile.velocity.Y)
+                {
+
+                    Projectile.netUpdate = true;
+                }
+
+
+                Projectile.velocity = holdoutOffset;
+            }
+            else
+            {
+              
+
+                SoundEngine.PlaySound(SoundID.Item5 with { Pitch = ( -1/4 + (Projectile.ai[0] / 120)), Volume = (Projectile.ai[0] / 30) });
+              
+                Projectile.NewProjectile(player.GetSource_FromThis(), player.Center, (Projectile.velocity / 2 + (Projectile.velocity * (Projectile.ai[0] / 120)) ) , (int)Projectile.ai[2], (int)(Projectile.damage /2 + (Projectile.damage  * (Projectile.ai[0] / 45))), 0f, player.whoAmI, player.whoAmI);
+                Projectile.Kill();
+
+            }
+        }
+
+        if (Projectile.velocity.X > 0f)
+        {
+            player.ChangeDir(1);
+        }
+        else if (Projectile.velocity.X < 0f)
+        {
+            player.ChangeDir(-1);
+        }
+
+        Projectile.spriteDirection = Projectile.direction;
+        player.ChangeDir(Projectile.direction); 
+        player.heldProj = Projectile.whoAmI; 
+        player.SetDummyItemTime(2); 
+        Projectile.Center = playerCenter; 
+        Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        player.itemRotation = (Projectile.velocity * Projectile.direction).ToRotation();
+
+
+
+    }
+
 }
